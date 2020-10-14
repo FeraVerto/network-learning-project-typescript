3031aec050ce30b06245ac9c3d13081cd277e27f
Клевакин Артем Николаевич <klevakin@tochka.com>
2020-10-14 15:15:35 +0500

    lesson 12, component breakdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Profile from "./components/Profile/Profile";
 
 const App = () => {
     return (
         <div className='app-wrapper'>
-            <header className='header'>
-                <img src="" alt=""/>
-            </header>
-
-            <nav className='nav'>
-                <ul>
-                    <li><a>Profile</a></li>
-                    <li><a>Messages</a></li>
-                    <li><a>News</a></li>
-                    <li><a>Music</a></li>
-                    <li><a>Settings</a></li>
-                </ul>
-            </nav>
-
-            <main className='main'>
-                <div>
-                    <img className='main-img'
-                         src="https://www.meme-arsenal.com/memes/0e09ef79001e05420d2cf37ae1f246c3.jpg" alt="Инженер"/>
-                </div>
-                <div>
-                    ava + descriptions
-                </div>
-                <div>
-                    My posts
-                    <div>
-                        New Post
-                    </div>
-                    <div>
-                        <div>Post 1</div>
-                        <div>Post 2</div>
-                    </div>
-                </div>
-            </main>
-
+            <Header/>
+            <Navbar/>
+            <Profile/>
         </div>
     );
 }
---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export const Profile = () => {
+    return (
+        <main className='main'>
+            <div>
+                <img className='main-img'
+                     src="https://www.meme-arsenal.com/memes/0e09ef79001e05420d2cf37ae1f246c3.jpg" alt="Инженер"/>
+            </div>
+            <div>
+                ava + descriptions
+            </div>
+            <div>
+                My posts
+                <div>
+                    New Post
+                </div>
+                <div>
+                    <div>Post 1</div>
+                    <div>Post 2</div>
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default Profile;
